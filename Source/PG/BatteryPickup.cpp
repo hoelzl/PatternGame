ce92a1f2180d653e5677caa95459db403c03dfd0
Matthias Hoelzl <tc@xantira.com>
2015-11-17 23:07:37 +0100

    Reduce visual quality, increase battery power

--- Source/PG/BatteryPickup.cpp
+++ Source/PG/BatteryPickup.cpp
@@ -7,7 +7,7 @@
 
 ABatteryPickup::ABatteryPickup() :
 	// The base power level of the battery
-	BatteryPower{ 250.f }
+	BatteryPower{ 750.f }
 {
 	// Assign a default static mesh
 	static auto StaticMeshName = TEXT("StaticMesh'/Game/ExampleContent/Blueprint_Communication/Meshes/SM_Battery_Medium.SM_Battery_Medium'");
