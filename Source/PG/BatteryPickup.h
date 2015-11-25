31874f092730921bdb2681f7ff03f93237b822b8
Matthias Hoelzl <tc@xantira.com>
2015-11-25 05:26:16 +0100

    Revert errors introduced by changing file encodings.

--- Source/PG/BatteryPickup.h
+++ Source/PG/BatteryPickup.h
@@ -1,1 +1,20 @@
-꿢膃냧ꖲ꟦ꆴ꣦販닣肱곢膍듧醨ꇧ負菫馬곢날곦날ꧦ鶨돢膒돦閲ꗦ邮諠뒊냧覡귦蒠껦趥諠뒊ꧦ릣뗦醥ꋥ膯ꗧ覃ꇦ릧껦鶐ꏦ궵껦ꂢ諢趩ꏦ놵ꗢ肢ꇧ醴닧ꖐꏦ궵껦鶥ꗧ覡ꗦ邮ꋠ뒊諢벪跠ꢠꃤ蒠ꧦ趫냢膴ꇧ邠닦붶ꓦ閳냦붷닢膡ꓢ膩ꃧ馩뗦薬맢膲냧覥ꗦ릴ꓢ膡ꃦ覡듦閲跠ꢠ꿠뒊菤놁鏢ꂩ諦趬돧負蟥북觢膁ꇧ醴닧ꖐꏦ궵ꃣꢠ뗦覬ꏢ膁꿧鶥菦ꆡ꟦ꖮ郦ꖣ뗧肍믠뒊蟤閎鋤薔蓥붂蓥꒨跠ꢉ諧膵곦ꖣ跠ꢉ꿢膓듧負ꗦ馡곧邠ꇦ놵돢膦닢膴ꧧ負ꇧ醴닧꒧ꃧ膲냦閲ꧦ閳諠ꖁꇧ醴닧ꖐꏦ궵꣢꒻諠뒊믠뒊
+// Copyright (c) 2015, Matthias Hölzl, All Rights Reserved.
+
+#pragma once
+
+#include "PowerChangingPickup.h"
+#include "BatteryPickup.generated.h"
+
+/**
+ * A pickup that provides power and is visually represented as battery
+ */
+UCLASS()
+class PG_API ABatteryPickup : public APowerChangingPickup
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this battery's properties
+	ABatteryPickup();
+
+};
